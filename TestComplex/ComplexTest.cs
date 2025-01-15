629028967e1224833b5dc53e813129d875aab4c7
Сергей Владиславович Маклецов <smak.80@yandex.ru>
2025-01-15 12:59:40 +0300

    Добавьте файлы проекта.

---
+++ TestComplex/ComplexTest.cs
@@ -0,0 +1,68 @@
+﻿using TestExample;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace TestComplex
+{
+    public class ComplexTest
+    {
+        [Fact]
+        public void ToString_ReturnsString()
+        {
+            var z1 = new Complex(3, 5);
+            Assert.Equal("3+5i", z1.ToString());
+            var z2 = new Complex(-3, -5);
+            Assert.Equal("-3-5i", z2.ToString());
+            var z3 = new Complex(0, 0);
+            Assert.Equal("0", z3.ToString());
+        }
+
+        [Theory]
+        [InlineData(3, 5, "3+5i")]
+        [InlineData(-3, -5, "-3-5i")]
+        [InlineData(0, 0, "0")]
+        [InlineData(1, 1, "1+i")]
+        [InlineData(-1, -1, "-1-i")]
+        [InlineData(0, 1, "i")]
+        [InlineData(1, 0, "1")]
+        [InlineData(0, -3, "-3i")]
+        [InlineData(0, 3, "3i")]
+        [InlineData(3, 0, "3")]
+        [InlineData(-3, 0, "-3")]
+        public void ToStringReIm_RetunsString(double re, double im, string expected)
+        {
+            var z = new Complex(re, im);
+            Assert.Equal(expected, z.ToString());
+        }
+
+        public static IEnumerable<object[]> Complexes
+        {
+            get
+            {
+                var sr = new StreamReader("complex_test.txt", System.Text.Encoding.UTF8);
+                while (true){
+                    var data = sr.ReadLine()?.Split(',', ' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+                    if (data is not null)
+                    {
+                        Complex c;
+                        try
+                        {
+                            c = new Complex(double.Parse(data[0]), double.Parse(data[1]));
+                        }
+                        catch
+                        {
+                            c = new Complex();
+                        }
+                        yield return new object[] { c, data[2] };
+                    }
+                    else yield break;
+                }
+            }
+        }
+        [Theory]
+        [MemberData(nameof(Complexes))]
+        public void ToString_Complex_ReturnsString(Complex z, string expected)
+        {
+            Assert.Equal(expected, z.ToString());
+        }
+    }
+}
---
+++ TestExample/Complex.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Text;
+
+namespace TestExample
+{
+    public class Complex
+    {
+        public double Re { get; set; }
+        public double Im { get; set; }
+
+        public Complex() { }
+        public Complex(double re, double im) { Re = re; Im = im; }
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            if (Re != 0.0 || Im == 0.0) sb.Append(Re);
+            if (Im != 0.0)
+            {
+                sb.Append(Im < 0 ? "-" : (Re != 0.0 ? "+" : ""));
+                if (Math.Abs(Im) != 1.0) sb.Append(Math.Abs(Im));
+                sb.Append('i');
+            }
+            return sb.ToString();
+        }
+    }
+}
